9520bbb44a4e4550068894d922c8fd7108243b37
v_nemtsev <v_nemtsev@in-line.ru>
2018-12-14 16:08:18 +0300

    first commit

---
+++ roistatTest.php
@@ -0,0 +1,43 @@
+<?php
+
+function revertPunctuationMarks($str = null) {
+    
+    if(empty($str))
+        return $str;
+    
+    $marksCount = preg_match_all("/[^\w\d ]/", $str, $matches, PREG_OFFSET_CAPTURE);
+    if($marksCount === false)
+    {
+        //mb exception needed
+        return $str;
+    }
+    
+    if($marksCount == 0)
+    {
+        return $str;
+    }
+    
+    $matches = $matches[0];
+    $matchesCount = count($matches);
+    for($i = 0; $i < ($matchesCount-1)/2; ++$i)
+    {
+        $str[$matches[$i][1]] = $matches[$matchesCount - 1 - $i][0];
+        $str[$matches[$matchesCount - 1 - $i][1]] = $matches[$i][0];
+    }
+    
+    return $str;
+}
+
+class Test extends TestCase {
+    public function test_revertPunctuationMarks($str, $revertStr) {
+        $this->assertEquals($revertStr, revertPunctuationMarks($str));
+    }
+    
+    public function provider()
+    {
+        return array(
+            array('', ''),
+            array('abc,d.e? f!', ''),
+        );
+    }
+}
